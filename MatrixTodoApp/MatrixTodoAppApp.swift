df4fb74374eb0529df05974838aba0c1eefcaa5d
raptarior <qazqww@naver.com>
2026-03-06 18:25:04 +0900

    Initial Commit

---
+++ MatrixTodoApp/MatrixTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixTodoAppApp.swift
+//  MatrixTodoApp
+//
+//  Created by Jingon Lee on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MatrixTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
